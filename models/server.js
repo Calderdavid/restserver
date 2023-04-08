d5da5c10a3829619313adf606f4cf0366a60dd5e
David Calderon <davidjcr200040@gmail.com>
2023-04-07 22:05:01 -0400

    config server updated

--- models/server.js
+++ models/server.js
@@ -43,7 +43,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(process.env.port, () => {
+        this.app.listen(this.port, () => {
             console.log('Servidor corriendo en puerto', this.port);
         })
     }
