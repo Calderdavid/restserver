7c01d1602bd8828830c239e3a5af96ddb8a301b7
David Calderon <davidjcr200040@gmail.com>
2023-04-08 01:24:06 -0400

    google Sign In

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,59 +1,111 @@
 const { response } = require('express');
+const bcryptjs = require('bcryptjs')
+
 const Usuario = require('../models/usuario');
-const bcryptjs = require('bcryptjs');
+
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+
 
 const login = async(req, res = response) => {
 
     const { correo, password } = req.body;
 
     try {
-
-        //Verificar si el email existe
-        const usuario = await Usuario.findOne({correo});
-
-        if(!usuario) {
+      
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if ( !usuario ) {
             return res.status(400).json({
-                msg: 'Usuario / Contraseña no son correctos - correo'
+                msg: 'Usuario / Password no son correctos - correo'
             });
         }
 
-        //Verificar si el usuario esta activo todavia en la base de datso ({stado: true})
-        //Verificar que el usuario no haya sido marcado como borrado ({status: false})
-        if(!usuario.estado) {
+        // SI el usuario está activo
+        if ( !usuario.estado ) {
             return res.status(400).json({
-                msg: 'Usuario / Contraseña no son correctos - estado: false'
+                msg: 'Usuario / Password no son correctos - estado: false'
             });
         }
 
-        //Verificar la contraseña (deberia hacer match con la contra de la DB)
-        const validPassword = bcryptjs.compareSync(password, usuario.password);
-        if(!validPassword) {
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, usuario.password );
+        if ( !validPassword ) {
             return res.status(400).json({
-                msg: 'Usuario / Contraseña no son correctos - password'
+                msg: 'Usuario / Password no son correctos - password'
             });
         }
 
-        //Generar JWT
-        const token = await generarJWT(usuario.id);
-
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
 
         res.json({
             usuario,
             token
         })
 
-
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
             msg: 'Hable con el administrador'
+        });
+    }   
+
+}
+
+
+const googleSignIn = async(req, res = response) => {
+
+    const { id_token } = req.body;
+    
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
         })
+
     }
+
+
+
 }
 
 
 
 module.exports = {
-    login
-};
+    login,
+    googleSignIn
+}
